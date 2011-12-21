8b47e25c07429eb7b405579a285737007fa2fa1c
ktf <not.discostoo@gmail.com>
2011-12-20 18:26:33 -0800

    enumerated lists don't break PDF compilation anymore

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# pandoc to convert from markdown to TeX, then my "nspify" script to make the
-# TeX a little bit more friendly to the nostarch package.
 echo "% this is automatically generated, don't mess with it" > chapter_list.tex
 for f in *.txt
 do
@@ -13,11 +11,11 @@ do
   sed -f nspify.sed <${outfile} >${outfile}.tmp
   mv -f ${outfile}.tmp ${outfile}
 
-  # include all chapters in chapter_list
+  # include chapters in chapter_list
   echo "\input{${outfile}}" >> chapter_list.tex
   mv -f ${outfile} output
 done
-mv chapter_list.tex output
+mv -f chapter_list.tex output
 
 # build the PDF
 cd output
